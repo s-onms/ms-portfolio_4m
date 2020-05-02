c80a4f4cdbedb344ac1ab99edee338f90d47d16e
s-onms <sok5lunaamspectgdmatu@outlook.jp>
2020-05-03 00:57:10 +0900

    unicorn.rb修正（Nginxの導入と設定)

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -11,7 +11,10 @@ working_directory app_path
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
-listen 3000
+# listen 3000
+# ↓修正
+listen "#{app_path}/tmp/sockets/unicorn.sock"
+
 
 #エラーのログを記録するファイルを指定
 stderr_path "#{app_path}/log/unicorn.stderr.log"
