b70e28b2651a082e572740828dece1526256c708
s-onms <sok5lunaamspectgdmatu@outlook.jp>
2020-05-07 23:43:53 +0900

    登録情報（カラム追加）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_20_025708) do
+ActiveRecord::Schema.define(version: 2020_05_07_143951) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2020_04_20_025708) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nickname"
+    t.text "first_name"
+    t.text "kana"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
