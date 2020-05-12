1e25c5087f66840fab7b62ca68e4abae413d7e31
s-onms <sok5lunaamspectgdmatu@outlook.jp>
2020-05-13 01:05:54 +0900

    非同期通信

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,21 @@
 class CommentsController < ApplicationController
 
   def create
-    comment = Comment.create(comment_params)
+    # comment = Comment.create(comment_params)
     # Comment.create(comment_params)
     # ↑大文字、小文字
     # ↓に書き換え 理由？
-    # binding.pry
-
-    
     # これから作るから、find（IDの箱を見つけ出す）やIDは不要
     # これから、コメントの箱を作るというイメージ
-
-    redirect_to "/tweets/#{comment.tweet.id}"
+    # redirect_to "/tweets/#{comment.tweet.id}"
     # コメントと結びつく詳細画面に遷移させる
+
+    # 非同期通信で下記に書き換え
+    @comment = Comment.create(comment_params)
+    respond_to do |format|
+      format.html { redirect_to tweet_path(params[:tweet_id])  }
+      format.json
+    end
 
   end
 
